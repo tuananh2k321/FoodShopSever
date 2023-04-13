d51e4586f8409f7b2f25062c33cc8884d69e6c79
tuananh2k321 <haizzj123@gmail.com>
2023-04-13 20:24:32 +0700

    index login register

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,19 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+// http://localhost:3000/
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
 });
+
+// http://localhost:3000/login
+router.get('/login', function(req, res, next) {
+  res.render('user/login')
+})
+
+// http://localhost:3000/register
+router.get('/register', function(req, res, next) {
+  res.render('user/register')
+})
 
 module.exports = router;
